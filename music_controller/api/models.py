62bb64837d776098b27529852cf59a8a39f6c374
jlee627 <32377243+jlee627@users.noreply.github.com>
2021-06-23 05:50:05 -0700

    setting up babel, react, webpack and material

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import string
+import random
+
+
+def generate_unique_code():
+  length = 6
+
+  while True:
+    code = ''.join(random.choices(string.ascii_uppercase, k=length)) # generate random code that is K length that only contains uppercase ascii characters
+    # Get list of all Room objects with their code matching this code, if none are found return this code
+    if Room.objects.filter(code=code).count() == 0:
+      break
+    
+  return code
 
 # Create your models here.
+class Room(models.Model):
+  code = models.CharField(max_length=8, default="", unique=True)
+  host = models.CharField(max_length=50, unique=True)
+  guest_can_pause = models.BooleanField(null=False, default=False) # By setting null=False we require a selection
+  votes_to_skip = models.IntegerField(null=False, default=1)
+  created_at = models.DateTimeField(auto_now_add=True) # Automatically add the date and time room was created
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,3 +1,11 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 # Create your views here.
+
+# Allows to view all rooms and create a room
+class RoomView(generics.ListAPIView):
+  queryset = Room.objects.all()
+  serializer_class = RoomSerializer
